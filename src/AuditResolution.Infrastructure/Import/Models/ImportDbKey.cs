4a1af3d25f3ad9cc2a917c41096966e294a221ed
Rick Flagg <Richard.Lee.Flagg@outlook.com>
2022-11-16 16:48:19 -0500

    Adding Import Parser

---
+++ src/AuditResolution.Infrastructure/Import/Models/ImportDbKey.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuditResolution.Infrastructure.Import.Models
+{
+    public class ImportDbKey
+    {
+        [JsonProperty("Item1")]
+        public int DbKey { get; set; }
+        [JsonProperty("Item2")]
+        public short AuditYear { get; set; }
+    }
+}
---
+++ src/AuditResolution.ReportDashboard/Configuration/ServiceConfiguration.cs
@@ -0,0 +1,45 @@
+﻿using AuditResolution.Infrastructure.Configuration;
+using AuditResolution.Infrastructure.Import.Parsers;
+
+namespace AuditResolution.ReportDashboard.Configuration
+{
+    public static class ServiceConfiguration
+    {
+        public static void ConfigureRequiredDependencies(this WebApplicationBuilder builder)
+        {
+            builder.Services.ConfigureDataRepositories();
+            builder.Services.ConfigureUtilities();
+            builder.Services.ConfigureLocalServices();
+            builder.ConfigureSettings();
+        }
+
+        private static void ConfigureParsers(this WebApplicationBuilder builder)
+        {
+            builder.AddHttpClient();
+            services.AddTransient<DbKeyParser>();
+        }
+
+        private static void ConfigureSettings(this WebApplicationBuilder builder)
+        {
+            builder.Services.Configure<AppSettings>(
+                builder.Configuration.GetSection("AppSettings")
+            );
+        }
+
+        private static void ConfigureDataRepositories(this IServiceCollection services)
+        {
+            
+        }
+
+        private static void ConfigureUtilities(this IServiceCollection services)
+        {
+            
+        }
+
+        private static void ConfigureLocalServices(this IServiceCollection services)
+        {
+           
+
+        }
+    }
+}
