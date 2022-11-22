ef4701903c0eca0fbd1baf4e68b2af3c9c7231db
Rick Flagg <Richard.Lee.Flagg@outlook.com>
2022-11-22 17:09:34 -0500

    Adding data entities

---
+++ src/services/AuditImportService/Data/Entities/AddtionalEIN.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AuditImportService.Data.Entities;
+
+[Table("ADDITIONAL_EINS")]
+public class AddtionalEIN
+{
+    [Key, Column("ID")]
+    public int Id { get; set; }
+    
+    [Column("EIN")]
+    public string EIN {get;set;}
+    
+    [Column("SEQNUM")]
+    public short SeqNum {get;set;}
+   
+    [Column("MAIN_EIN")]
+    public string? MainEIN {get;set;}
+}
