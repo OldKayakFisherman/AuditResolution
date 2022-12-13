d5b205e67f7f9376788fcf7fb3ef1c0f0f9de495
Rick Flagg <richard.lee.dlagg@outlook.com>
2022-12-12 19:06:16 -0500

    Add required migrations

---
+++ src/services/AuditImportService/Migrations/20221212230942_AddDbKeyImportLog.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuditImportService.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDbKeyImportLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DBKEY_IMPORT_LOG",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DBKEY = table.Column<int>(type: "INTEGER", nullable: false),
+                    AUDITYEAR = table.Column<short>(type: "INTEGER", nullable: false),
+                    DATEADDED = table.Column<DateTime>(name: "DATE_ADDED", type: "TEXT", nullable: false),
+                    DATEIMPORTED = table.Column<DateTime>(name: "DATE_IMPORTED", type: "TEXT", nullable: true),
+                    PDFIMPORTED = table.Column<bool>(name: "PDF_IMPORTED", type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DBKEY_IMPORT_LOG", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DBKEY_IMPORT_LOG");
+        }
+    }
+}
