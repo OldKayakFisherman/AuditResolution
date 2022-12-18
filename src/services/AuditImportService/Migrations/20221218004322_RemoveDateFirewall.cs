d6f7e9a33c385bcb2997e74409f5597bbfa350cf
Rick Flagg <richard.lee.dlagg@outlook.com>
2022-12-17 19:46:12 -0500

    Removed DateFirewall from the entity mapping.

---
+++ src/services/AuditImportService/Migrations/20221218004322_RemoveDateFirewall.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuditImportService.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveDateFirewall : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FAC_FIREWALL_DATE",
+                table: "AUDIT_TIMEFRAMES");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "FAC_FIREWALL_DATE",
+                table: "AUDIT_TIMEFRAMES",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
